6036b0007bde2af4223bd512f2c4ed75ac7bcdee
munuiee <munuiee@icloud.com>
2025-10-13 15:02:16 +0900

    [CHORE] 코드베이스 설정 및 폴더링

---
+++ KickMe/KickMe/Controller/MapViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
