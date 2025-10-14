a6be514f0ad0cc89808a6e234022ddbf6859894d
munuiee <munuiee@icloud.com>
2025-10-14 14:36:43 +0900

    [Feat] #1 네비게이션바 생성

--- KickMe/KickMe/Controller/MapViewController.swift
+++ KickMe/KickMe/Controller/MapViewController.swift
@@ -1,10 +1,22 @@
 import UIKit
 
 class MapViewController: UIViewController {
+    private let customBar = navigationBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray6
+        configureBarUI()
+    }
+    
+    func configureBarUI() {
+        view.addSubview(customBar)
+        customBar.snp.makeConstraints {
+            $0.width.equalTo(250)
+            $0.height.equalTo(60)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(20) // 안전 영역 기준으로 하단 고정
+        }
     }
 
 
