6817284bb616bfe0cdc1bf4192c1ecf6847b3258
RiHA039 <riyaya0320@gmail.com>
2025-10-14 17:52:04 +0900

    [Feat] #3 로그인 화면

--- KickMe/KickMe/Controller/MapViewController.swift
+++ KickMe/KickMe/Controller/MapViewController.swift
@@ -5,6 +5,7 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
