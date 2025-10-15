f097585f1f4fc1328a689dbd71c450924eccd8ca
RiHA039 <riyaya0320@gmail.com>
2025-10-15 20:45:37 +0900

    Fix

--- KickMe/KickMe/Supporting Files/SceneDelegate.swift
+++ KickMe/KickMe/Supporting Files/SceneDelegate.swift
@@ -9,7 +9,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
 
-        window.rootViewController = MapViewController()
+
+        window.rootViewController = TabBarController()
+
         window.makeKeyAndVisible()
 
         self.window = window
